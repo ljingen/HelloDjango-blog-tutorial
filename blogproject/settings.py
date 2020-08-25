a24774c234ef34f3ba43f6fa3b1e673c5cd5f50a
luojingen <luojingen@aliyun.com>
2020-08-25 18:43:14 +0800

    http://81.70.51.41/

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -29,7 +29,7 @@ DEBUG = False
 
 # ALLOWED_HOSTS = []
 # 线上环境使用下面配置
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost ', '.zmrenwu.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost ', '.zmrenwu.com', '81.70.51.41']
 
 # Application definition
 
